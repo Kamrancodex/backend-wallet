d478f64fce37c7655e115dc4e2eaab47b2d012ec
Kamrancodex <najarkamran212@gmail.com>
2024-06-25 21:00:11 +0530

    cors setting

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.get("/", function (req, res) {
